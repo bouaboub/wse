3f2f5bcfb82a077863fa673500fe593cae3934fd
mohamed <mohbouaboub@gmail.com>
2018-02-20 15:25:12 -0500

    first commit

---
+++ src/app/animations/animations.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatFormFieldModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatAutocompleteModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatDatepickerModule,
+    
+  ],
+  exports: [
+      
+      MatDatepickerModule,
+       
+  ],
+  declarations: []
+})
+export class AnimationsModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AnimationsModule } from "./animations/animations.module";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ClientComponent } from './client/client.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+
+
+const routeLists : Routes = [
+  {path:"", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"services", component:ServicesComponent},
+  {path:"technologies", component:TechnologiesComponent},
+  {path:"client", component:ClientComponent},
+  {path:"contact", component:ContactComponent}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    TechnologiesComponent,
+    ClientComponent,
+    ServicesComponent,
+    FooterComponent,
+    AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(routeLists),
+    ReactiveFormsModule,
+    HttpClientModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  exports:[
+    BrowserModule,
+    BrowserAnimationsModule,
+    AnimationsModule,
+    ReactiveFormsModule,
+    MDBBootstrapModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
